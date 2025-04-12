be510b56447d3d238a392341e770f58ad5bbc64d
Nícolas Carvalho <nicolassprofissional@gmail.com>
2025-04-12 10:45:39 -0300

    feat: exercises class 02

---
+++ exercicios_de_aula/aula_02.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+int main() {
+    // printf("\n============================ QUESTÃO 01 ============================\n");
+    // int a, b, c;
+    
+    // printf("Digite três números inteiros quaisquer: ");
+    // scanf("%d %d %d", &a, &b, &c);
+
+    // if (a < b && b < c) printf("A afirmação %d < %d < %d é verdadeira\n", a, b, c);
+    // else printf("A afirmação %d < %d < %d é falsa\n", a, b, c);
+    
+
+
+    // printf("\n============================ QUESTÃO 02 ============================\n");
+    // int a, b, c, d;
+    
+    // printf("Digite quatro números inteiros quaisquer: ");
+    // scanf("%d %d %d %d", &a, &b, &c, &d);
+
+    // if (a < b && b < c && c < d) printf("A afirmação %d < %d < %d < %d é verdadeira\n", a, b, c, d);
+    // else printf("A afirmação %d < %d < %d < %d é falsa\n", a, b, c, d);
+
+
+
+    // printf("\n============================ QUESTÃO 03 ============================\n");
+    // int a, b, c;
+
+    // printf("Digite três números inteiros quaisquer: ");
+    // scanf("%d %d %d", &a, &b, &c);
+
+    // if (b-a == c-b) printf("%d, %d, %d é uma progressão aritmética\n", a, b, c);
+    // else printf("%d, %d, %d não é uma progressão aritmética\n", a, b, c);
+
+
+
+    // printf("\n============================ QUESTÃO 04 ============================\n");
+    // int a, b, c;
+
+    // printf("Digite três números inteiros quaisquer: ");
+    // scanf("%d %d %d", &a, &b, &c);
+
+    // if (b/a == c/b) printf("%d, %d, %d é uma progressão geométrica\n", a, b, c);
+    // else printf("%d, %d, %d não é uma progressão geométrica\n", a, b, c);
+
+    
+
+    // printf("\n============================ QUESTÃO 05 ============================\n");
+    // int d1, m1, a1, d2, m2, a2;
+
+    // printf("Digite duas datas quais quer seguindo a ordem DIA/MÊS/ANO: ");
+    // scanf("%d %d %d %d %d %d", &d1, &m1, &a1, &d2, &m2, &a2);
+
+    // if (a1 >= a2 && m1 >= m2 && d1 > d2) printf("%d/%d/%d é a data mais recente\n", d1, m1, a1);
+    // else printf("%d/%d/%d é a data mais recente\n", d2, m2, a2);
+
+    return 0;
+}
