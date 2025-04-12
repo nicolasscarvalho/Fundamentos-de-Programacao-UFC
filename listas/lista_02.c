ac1ac499a8d2716ccad4af0ff6acb74e2ac11ee7
Nícolas Carvalho <nicolassprofissional@gmail.com>
2025-04-12 10:46:33 -0300

    feat: exercise list 02

---
+++ listas/lista_02.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+
+int main() {
+    // printf("\n============================ QUESTÃO 01 ============================\n");
+    // int a, b, c, d, maior;
+
+    // printf("Digite quatro inteiros quaisquer: ");
+    // scanf("%d %d %d %d", &a, &b, &c, &d);
+
+    // if (a > b) {maior=a; a=b, b=maior;};
+    // if (a > c) {maior=a; a=c, c=maior;};
+    // if (a > d) {maior=a; a=d, d=maior;};
+
+    // if (b > c) {maior=b; b=c, c=maior;};
+    // if (b > d) {maior=b; b=d, d=maior;};
+
+    // if (c > d) {maior=c; c=d, d=maior;};
+
+    // printf("%d é o maior número\n", d);
+
+
+
+    // printf("\n============================ QUESTÃO 02 ============================\n");
+    // int a = 5, b = 2, c = 7, d = 8;
+    // int crescente;
+
+    // if (a<b && b<c && c<d) crescente=1;
+    // else crescente=0;
+
+    // printf("Valor da variável crescente: %d\n", crescente);
+
+
+
+    // printf("\n============================ QUESTÃO 01 ============================\n");
+    // int a, b, c, d, maior;
+
+    // printf("Digite quatro inteiros quaisquer: ");
+    // scanf("%d %d %d %d", &a, &b, &c, &d);
+
+    // if (a > b) {maior=a; a=b, b=maior;};
+    // if (a > c) {maior=a; a=c, c=maior;};
+    // if (a > d) {maior=a; a=d, d=maior;};
+
+    // if (b > c) {maior=b; b=c, c=maior;};
+    // if (b > d) {maior=b; b=d, d=maior;};
+
+    // if (c > d) {maior=c; c=d, d=maior;};
+
+    // printf("Em ordem crescente, a lista ficaria %d, %d, %d e %d", a, b, c, d);
+
+
+
+    // printf("\n============================ QUESTÃO 01 ============================\n");
+    // float base, altura, area;
+
+    // printf("Digite os respectivos valores para base e altura de um triângulo: ");
+    // scanf("%f %f", &base, &altura);
+
+    // area = base * altura / 2;
+
+    // printf("A área desse triângulo equvale a %f\n", area);
+
+    return 0;
+}
