9303c8341568e6edaedf81de4115421e231e6e7e
Nícolas Carvalho <nicolassprofissional@gmail.com>
2025-04-12 10:46:27 -0300

    feat: exercise list 01

---
+++ listas/lista_01.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+int main() {
+    // printf("\n============================ QUESTÃO 01 ============================\n");
+    // float base, altura;
+
+    // printf("Digite os respectivos valores para base e altura de um triângulo: ");
+    // scanf("%f %f", &base, &altura);
+
+    // printf("A área desse triângulo equvale a %f\n", base * altura / 2);
+
+
+
+    // printf("\n============================ QUESTÃO 02 ============================\n");
+    // float n1, n2, n3, auxiliar;
+
+    // printf("Digite três notas quaisquer: ");
+    // scanf("%f %f %f", &n1, &n2, &n3);
+
+    // if (n1>n2){auxiliar = n1; n1=n2; n2=auxiliar;}
+    // if (n1>n3){auxiliar = n1; n1=n3; n3=auxiliar;}
+    // if (n2>n3){auxiliar = n2; n2=n3; n3=auxiliar;}
+
+    // if((n2+n3)/2 >= 7) printf("Aluno aprovado\n");
+    // else printf("Aluno reprovado\n");
+
+
+
+    // printf("\n============================ QUESTÃO 03 ============================\n");
+    // float x, y, px, py;
+    
+    // printf("Digite valores para Px, X, Py e Y, sendo X e Y dois números quais quer e Px e Py seus respectivos pesos: ");
+    // scanf("%f %f %f %f", &px, &x, &py, &y);
+
+    // if ((px + py) != 0) printf("A média ponderada entre esses números é de %f \n", (px * x + py * y) / (px+py));
+    // else printf("Os pesos utilizados são inválidos \n");
+
+
+
+    // printf("\n============================ QUESTÃO 04 ============================\n");
+    // float n1, n2, n3, auxiliar;
+
+    // printf("Digite três notas quaisquer: ");
+    // scanf("%f %f %f", &n1, &n2, &n3);
+
+    // if (n1>n2){auxiliar = n1; n1=n2; n2=auxiliar;}
+    // if (n1>n3){auxiliar = n1; n1=n3; n3=auxiliar;}
+    // if (n2>n3){auxiliar = n2; n2=n3; n3=auxiliar;}
+
+    // if((2*n1+n3)/3 >= 7) printf("Aluno aprovado\n");
+    // else printf("Aluno reprovado\n");
+
+    return 0;
+}
