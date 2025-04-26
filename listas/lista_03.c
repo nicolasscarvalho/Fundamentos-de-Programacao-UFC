f16aa0b00c182fda76962187aa11a432492c9685
Nícolas Carvalho <nicolassprofissional@gmail.com>
2025-04-25 22:38:31 -0300

    fix: exercise list 03

--- listas/lista_03.c
+++ listas/lista_03.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 
-#define N 10
-
 int main() {
     // printf("\n============================ QUESTÃO 01 ============================\n");
-    // int L[N] = {1,2,3,4,5,6,7,8,9,10};
+    // int L[] = {1,2,3,4,5,6,7,8,9,10};
 
     // float media;
     
@@ -21,19 +19,19 @@ int main() {
 
 
     // printf("\n============================ QUESTÃO 02 ============================\n");
-    // int L[N] = {1,2,3,4,5,6,7,8,9,10};
+    // int L[] = {1,2,3,4,5,6,7,8,9,10};
 
     // float media = 0.0;
-    // int i, mm;
+    // int mm;
     
-    // for (i = 0; i < N; i++) {
+    // for (int i = 0; i < N; i++) {
     //     media += (float)L[i];
     // }
     // media /= N;
 
 
     // mm = 0;
-    // for (i = 0; i < N; i++) {
+    // for (int i = 0; i < N; i++) {
     //     if (L[i] > media) mm ++;
     // }
 
@@ -42,7 +40,7 @@ int main() {
 
 
     // printf("\n============================ QUESTÃO 03 ============================\n");
-    // int L[N] = {1,2,1,3,1,4,1,5,1,6};
+    // int L[] = {1,2,3,4,5,6,7,8,9,10};
     // int resposta;
 
     // resposta = 0;
@@ -56,33 +54,112 @@ int main() {
 
 
     // printf("\n============================ QUESTÃO 04 ============================\n");
-    int L[N] = {1,2,3,4,5,6,7,8,9,10};
-    int mimpar, smimpar, temp;
+    // int L[] = {1,2,3,4,5,6,7,8,9,10};
+    // int mimpar;
+    
+    // mimpar = -1;
+    
+    // for (int i = 0; i < N; i++) {
+    //     if (L[i] % 2 == 1) {
+    //         if(mimpar == -1) mimpar = L[i];
+    //         else if(L[i] < mimpar) mimpar = L[i];
+    //     }
+    // }
 
-    mimpar, smimpar = -1, -1;
+    // printf("Menor ímpar: %d", mimpar);
 
 
-    for (int i=0; i < N; i++) {
-        for (int j=i; j < N; j++){
-            if (L[i] > L[j]){
-                temp = L[i];
-                L[i] = L[j];
-                L[j] = temp;
-            }
-        }
-    } 
 
-    mimpar = L[0];
-    int encontrados = 0;
+    // printf("\n============================ QUESTÃO 05 ============================\n");
+    // int L[] = {1,2,3,4,5,6,7,8,9,10};
+    // int mimpar, smimpar, temp;
 
-    for (int i = 1; i < N; i++) {
-        if (L[i] < mimpar && encontrados == 2) {
-            smimpar = L[i];
-            break;
-        }
-    }
+    // mimpar = -1;
+    // smimpar = -1;
 
-    printf("Menor ímpar: %d, Segundo ímpar menor: %d\n", mimpar, smimpar);
+    // for (int i = 0; i < N-1; i++) {
+    //     for (int j = i+1; j < N; j++){
+    //         if (L[i] > L[j]) {
+    //             temp = L[i];
+    //             L[i] = L[j];
+    //             L[j] = temp;
+    //             continue;
+    //         }
+    //     }
+    // }
+
+    // for (int i = 0; i < N; i++){
+    //     if (L[i] % 2 == 1) {
+
+    //         if (mimpar != -1 && L[i] > mimpar) {
+    //             smimpar = L[i];
+    //             break;
+    //         }
+
+    //         mimpar = L[i];
+
+    //     }
+    // }
+
+    // printf("Segundo ímpar menor: %d\n", smimpar);
+
+
+
+    // printf("\n============================ QUESTÃO 06 ============================\n");
+    // int L[] = {1,2,3,4,5,6,7,8,9,10};
+    // int tmaior, temp, counter;
+
+    // for (int i = 0; i < N-1; i++) {
+    //     for (int j = i+1; j < N; j++){
+    //         if (L[i] > L[j]) {
+    //             temp = L[i];
+    //             L[i] = L[j];
+    //             L[j] = temp;
+    //             continue;
+    //         }
+    //     }
+    // }
+
+    // counter = 0;
+
+    // for (int i = N-1; i >= 0; i--) {
+    //     tmaior = L[i];
+    //     counter ++;
+
+    //     if (counter == 3) break;
+    // }
+
+    // printf("Terceiro maior: %d\n", tmaior);    
+
+
+
+    // printf("\n============================ QUESTÃO 07 ============================\n");
+    // int L[] = {1,2,3,4,5,6,7,8,9,10};
+    // int temp;
+
+    // int k = 79;
+    // int mmk = -1;
+
+    // for (int i = 0; i < N-1; i++) {
+    //     for (int j = i+1; j < N; j++){
+    //         if (L[i] > L[j]) {
+    //             temp = L[i];
+    //             L[i] = L[j];
+    //             L[j] = temp;
+    //             continue;
+    //         }
+    //     }
+    // }
+
+
+    // for (int i = N-1; i >= 0; i--) {
+    //     if (L[i] < k) {
+    //         mmk = L[i];
+    //         break;
+    //     }
+    // }
+
+    // printf("Primeiro menor que K: %d\n", mmk);
 
     return 0;
 }
